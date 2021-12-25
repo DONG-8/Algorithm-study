d8dac1f277763114f4ce167ebc3fb2a292b9d02a
unknown <junzzamg9@gmail.com>
2021-12-26 01:00:11 +0900

    211226||BOJ_4485_update

--- 괴수 스터디/다지기4/9663_N-Queen.py
+++ 괴수 스터디/다지기4/9663_N-Queen.py
@@ -13,34 +13,32 @@ def recur(x):
 
     for i in range(N):
         if visit[x][i] == 0:
+            pass    
             
-            
-
-
-# 퀸이 있는 자리의 행과 열 각 대각선 방문처리 하는 함수
-def queen_gs(x,y):
-    # 행 & 열    
-    for i in range(N):
-        visit[i][y] = 1
-        visit[x][i] = 1
-
-    # 대삭선 교차 체크
-    dx = [1, -1, -1, 1]
-    dy = [1, 1, -1, -1]
-    q = deque()
-    q.append([x,y])
-    while q:
-        i,j = q.popleft()
-        for c in range(4):
-            cx = i + dx[c]
-            cy = j + dy[c]
-            if 0<= cx < N and 0<= cy < N and visit[cx][cy] == 0:
-                visit[cx][cy] = 1
-                q.append([cx,cy])
-
 N = int(input())
 visit = [[0]*N for _ in range(N)]
 
 
 num = 0
+
 recur(0)
+# # 퀸이 있는 자리의 행과 열 각 대각선 방문처리 하는 함수
+# def queen_gs(x,y):
+#     # 행 & 열    
+#     for i in range(N):
+#         visit[i][y] = 1
+#         visit[x][i] = 1
+
+#     # 대삭선 교차 체크
+#     dx = [1, -1, -1, 1]
+#     dy = [1, 1, -1, -1]
+#     q = deque()
+#     q.append([x,y])
+#     while q:
+#         i,j = q.popleft()
+#         for c in range(4):
+#             cx = i + dx[c]
+#             cy = j + dy[c]
+#             if 0<= cx < N and 0<= cy < N and visit[cx][cy] == 0:
+#                 visit[cx][cy] = 1
+#                 q.append([cx,cy])
