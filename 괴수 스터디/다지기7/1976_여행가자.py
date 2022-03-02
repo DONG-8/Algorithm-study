26036ca2cd6df31aecedbabcdfb5995dd8aefe2e
DONG-8 <junzzamg9@gmail.com>
2022-03-02 23:41:13 +0900

    220302||BOJ_1976_update

---
+++ 괴수 스터디/다지기7/1976_여행가자.py
@@ -0,0 +1,14 @@
+
+N = int(input())
+M = int(input())
+
+
+# 양방향 연결임
+for i in range(N):
+    a , b, connect = map(int, input().split())
+    # print(a,b,connect)
+root = list(map(int,input().split()))
+
+
+# print(root)
+
