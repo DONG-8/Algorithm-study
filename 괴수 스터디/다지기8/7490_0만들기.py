9f51147ca38d8a88dc60effcd53f98d9ee1d45d0
DONG-8 <junzzamg9@gmail.com>
2022-03-08 01:35:54 +0900

    220308||BOJ_7490_done

--- 괴수 스터디/다지기8/7490_0만들기.py
+++ 괴수 스터디/다지기8/7490_0만들기.py
@@ -1,11 +1,44 @@
+
 N  = int(input())
 
-for i in range(N):
-    a = int(input())
-    # 1~a 까지 수 오름차순으로 정렬
-
-    # 하나의 수가 체크 될때 더하기 혹은, -  혹은 더하기
-
-    list = ['+','-','x']
+def sum_of_st(st):
+    # str의 합 구하기!
+    a = ''.join(st)
+    a = a.replace(" ",'')
+    return eval(a)
 
     
+
+def recur(cur, st):
+    # 하나의 수가 체크 될때 더하기 혹은, -  혹은 더하기
+    arr = ['+','-',' ']
+
+    if cur == a:
+        if sum_of_st(st) == 0:
+            b = ''.join(st)
+            ls.append(b)
+        return
+
+    for x in arr:
+        st.append(x)
+        st.append(str(cur+1))
+        recur(cur+1,st)
+        st.pop()
+        st.pop()
+
+
+
+for i in range(N):
+    # 빈 리스트를 삽입 해 줍니다.
+    ls = []
+
+    a = int(input())
+    # 1~a 까지 수 오름차순으로 정렬
+    num = 0
+    recur(1,['1'])
+    # 1 ~ 입력받은 숫자까지 입력을 받음)
+    ls.sort()
+    for i in ls:
+        print(i)
+
+    print()
