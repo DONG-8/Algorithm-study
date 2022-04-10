a02413dff05624aa4fd3f3730b858159b769c48e
DONG-8 <junzzamg9@gmail.com>
2022-04-11 03:13:44 +0900

    220411 || JS_Algorithm 없는숫자더하기 done

---
+++ JS_ALGO/없는숫자더하기.js
@@ -0,0 +1,12 @@
+// 없는숫자 더하기
+// 배열에서 없는 숫자만의 list를 추출하여 더해준다.
+
+function solution(numbers) {
+  const result = numbers.re;
+  return numbers.reduce((acc, cur) => {
+    return acc - cur;
+  }, 45);
+}
+
+arr = [1, 2, 3, 4, 6, 7, 8, 0];
+console.log(solution(arr));
