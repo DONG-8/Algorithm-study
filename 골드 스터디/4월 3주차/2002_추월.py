07869c9d36a45fea3b40bbf97f396df45e51f2ac
DONG-8 <junzzamg9@gmail.com>
2022-04-16 13:21:12 +0900

    220416 || BOJ_2002_추월_doing

---
+++ 골드 스터디/4월 3주차/2002_추월.py
@@ -0,0 +1,14 @@
+n = int(input())
+total_arr = [input() for _ in range(2*n)]
+
+# in_Car = total_arr
+
+# a b c d e
+# b a d c e
+# a랑 b 가 다르다 추월
+# a가 나올때 까지는 체크 x
+# c d 다르네? 그럼 다시 c 나올때 까지 체크
+# 
+
+in_arr = total_arr[0:n]
+out_arr = total_arr[n:2*n]
