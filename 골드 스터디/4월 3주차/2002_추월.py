c9423694dbfc206d203ec35e7d906a33c8bc0979
DONG-8 <junzzamg9@gmail.com>
2022-04-19 00:32:34 +0900

    220419 || 추월_2002 done

--- 골드 스터디/4월 3주차/2002_추월.py
+++ 골드 스터디/4월 3주차/2002_추월.py
@@ -1,6 +1,4 @@
 n = int(input())
-total_arr = [input() for _ in range(2*n)]
-
 # in_Car = total_arr
 
 # a b c d e
@@ -9,9 +7,29 @@ total_arr = [input() for _ in range(2*n)]
 # a가 나올때 까지는 체크 x
 # c d 다르네? 그럼 다시 c 나올때 까지 체크
 # 
+join, end = dict(), []
 
-in_arr = total_arr[0:n]
-out_arr = total_arr[n:2*n]
+for i in range(n):
+    car = input()
+    join[car] = i
 
-for i in range(len(in_arr)):
-    print(in_arr[i],'나와떠염')
+for i in range(n):
+    car = input()
+    end.append(car)
+
+# print(join)
+count = 0
+
+for i in range(n-1):
+    for j in range(i+1,n):
+        # 나오게 된 차량부터 순서대로 확인
+        # 만약 나오게 된 차량의 들어갔을때의 인덱스 값보다 현재 차량 뒤에 나오는
+        # 차량의 들어갈때 인덱스 번호보다 크다면 추웧 한 것이므로 제거해줌
+        if join[end[i]] > join[end[j]]:
+            count += 1
+            # 이미 이 차가 추월했음을 확인했기 때문에 
+            break
+
+print(count)
+            
+        
